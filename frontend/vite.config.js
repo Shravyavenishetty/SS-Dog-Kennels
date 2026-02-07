7178d6f8bc5b5e59a3081c154461ddcc101b5c65
shravyavenishetty <shravyavenishetty03@gmail.com>
2026-02-07 17:03:24 +0530

    fix vite base path for github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "SS-Dog-Kennels/",
+  base: "/SS-Dog-Kennels/",
   plugins: [
     react(),
     tailwindcss(),
